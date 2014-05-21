75df46c110045918c8381cf8e19ebcd52521ce4f
sscnt <pixeltc@gmail.com>
2014-05-21 21:55:45 +0900

    Initial Commit

---
+++ Lumina/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Lumina
+//
+//  Created by SSC on 2014/05/21.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
